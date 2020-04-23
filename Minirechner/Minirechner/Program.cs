69ffaee0e90d64a1c85caecb3d1ceec242ad620a
HofLuk77 <02hofluk@rgtfo-me.it>
2020-04-23 23:15:03 +0200

    EINGABE F

--- Minirechner/Minirechner/Program.cs
+++ Minirechner/Minirechner/Program.cs
@@ -8,11 +8,15 @@ namespace Minirechner
     
     class Program
     {
-     
+
         static void Main(string[] args)
         {
+            Ausgabe();
+        }
+        static void Ausgabe()
+        {
             Console.WriteLine("Minirechner by HOFER Lukas 4A EL");
-            
+
             Console.WriteLine("Bitte gib Zahl1 ein:");
             int zahl1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Bitte gib Zahl2 ein:");
@@ -21,33 +25,28 @@ namespace Minirechner
             Console.WriteLine("\nWelche Rechenart soll ausgeführt werden?: [+,-,/,*]");
             string menu = Console.ReadLine();
 
-            Abfrage(menu, zahl1, zahl2);
-            
-            Console.ReadKey();
-        }
-        static void Abfrage(string menu, int zahl1, int zahl2)
-        {
             if (menu == "+")
             {
-                Addiere(int zahl1, int zahl2);
+                Add(zahl1, zahl2);  
             }
-            else if (menu == "-")
+            if (menu == "-")
             {
-                Subtrahiere(int zahl1, int zahl2);
+                Sub(zahl1, zahl2);
             }
-            else if (menu == "*")
+            if (menu == "*")
             {
-                Multipliziere(int zahl1, int zahl2);
+                Mul(zahl1, zahl2);
             }
-            else if (menu == "/")
+            if (menu == "/")
             {
-                Dividiere(int zahl1, int zahl2);
+                Div(zahl1, zahl2);
             }
             else
             {
                 Console.WriteLine("Bitte seien sie so nett und geben was sinnvolles ein DANKE");
             }
-
+          
+            Console.ReadKey();
         }
 
        
