ad61e409800c6ebd716215ea9553fae9c465f57a
HofLuk77 <02hofluk@rgtfo-me.it>
2020-04-23 21:40:00 +0200

    Eingabe

--- Minirechner/Minirechner/Program.cs
+++ Minirechner/Minirechner/Program.cs
@@ -5,10 +5,26 @@ using System.Text;
 
 namespace Minirechner
 {
+    
     class Program
     {
+     
         static void Main(string[] args)
         {
+            Console.WriteLine("Minirechner by HOFER Lukas 4A EL");
+            Console.WriteLine("\nWelche Rechenart soll ausgeführt werden?: [+,-,/,*]");
+            string menu = Console.ReadLine();
+
+            Console.WriteLine("Bitte gib Zahl1 ein:");
+            int zahl1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Bitte gib Zahl2 ein:");
+            int zahl2 = Convert.ToInt32(Console.ReadLine());
+            
+            Console.ReadKey();
         }
+
+       
+
+       
     }
 }
